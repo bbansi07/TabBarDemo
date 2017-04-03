5eb4cb975bd79f734e6e926187e5d592b7e8b7f5
Zeitech Solutions <zeitechsolutions@zeitech-mac.local>
2017-04-03 15:23:52 +0530

    Initial Commit

---
+++ TabBarDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TabBarDemo
+//
+//  Created by Zeitech Solutions on 03/04/17.
+//  Copyright © 2017 Bansi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
